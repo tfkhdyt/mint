51876fb940d8e9c9382a6fcdf28d9e970e498df9
Taufik Hidayat <taufik@generativestudio.dev>
2025-05-21 06:01:56 +0700

    fish: add yazi completions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,15 @@ function spf
     end
 end
 
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
 if status is-interactive
     alias ls="eza --icons --group-directories-first"
     alias ll="eza -lbh --icons --group-directories-first"
