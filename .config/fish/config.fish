3b8c20437e5f01a10228875a5837017ca4e4b6e1
Taufik Hidayat <taufik@generativestudio.dev>
2025-05-24 09:19:26 +0700

    chore: update fish config and add gitignore
    - Updates editor path in fish config
    - Adds imv alias
    - Adds gitignore for secrets.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -26,10 +26,11 @@ if status is-interactive
     abbr -a lg lazygit
     abbr -a p pnpm
     abbr -a g git
+    abbr -a imv imv-x11
 
     set -U fish_greeting ""
     set -gx GOPATH "$HOME/go"
-    set -gx EDITOR /home/tfkhdyt/.local/share/bob/nvim-bin/nvim
+    set -gx EDITOR /home/tfkhdyt/.local/bin/nvim
     set -gx PNPM_HOME "$HOME/.local/share/pnpm"
     set -gx BUN_INSTALL "$HOME/.bun"
     set -gx ANDROID_HOME "$HOME/Android"
@@ -38,7 +39,7 @@ if status is-interactive
 
     fish_add_path -a "$GOPATH/bin" "$HOME/.cargo/bin" "$BUN_INSTALL/bin" \
         "$JAVA_HOME/bin" "$ANDROID_HOME/cmdline-tools/latest/bin" "$ANDROID_HOME/platform-tools" \
-        "$ANDROID_HOME/emulator" "$HOME/.local/share/bob/nvim-bin" "$VOLTA_HOME/bin"
+        "$ANDROID_HOME/emulator" "$VOLTA_HOME/bin"
 
     # function cursor
     #     /home/tfkhdyt/Applications/Cursor-0.48.8-x86_64_92f79d3f09a90a9c3b62383539a52f74.AppImage $argv
